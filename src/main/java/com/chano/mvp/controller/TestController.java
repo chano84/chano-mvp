f52563177fe88f51743d91d839d7ba87ae9d9cdf
luciano.chambers <luciano.chambers@frubana.com>
2023-03-03 22:43:50 -0300

    Controller de test

--- src/main/java/com/chano/mvp/controller/TestController.java
+++ src/main/java/com/chano/mvp/controller/TestController.java
@@ -1,7 +1,14 @@
 package com.chano.mvp.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello word";
+    }
+
 }
